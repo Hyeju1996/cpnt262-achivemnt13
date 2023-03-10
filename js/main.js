993d3fa96bc4671bf124ba6cd06fd529c23264d5
helena <helena@helenas-MBP.hitronhub.home>
2023-03-10 13:53:46 -0800

    achivement

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// exersice 1
+
+let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+
+const reversed = arr.reverse();
+console.log(reversed);
+
+// exersice 2
+const n = 22;
+for (let i = 22; i <= 100; i++) {
+    console.log(i);
+}
+//exersice 3
+let integers = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
+console.log(integers);
+for (let i = 89; i <=100; i ++){
+     console.log(i);
+}
